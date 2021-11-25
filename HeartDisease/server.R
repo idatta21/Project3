37c598f26eed04b9ded4ce42bb938bea9e4a03c5
Ipsita Datta <suman.ipsita@gmail.com>
2021-11-25 14:56:18 -0500

    updated modeling info,fitting andprediction page

--- HeartDisease/server.R
+++ HeartDisease/server.R
@@ -6,11 +6,12 @@ library(DT)
 library(caret)
 library(tidyverse)
 library(plotly)
+library(rattle)
 
 
-data <- read_csv("heart_cleveland.csv", col_names = TRUE)
-
-heartDiseaseData<-data
+heartDiseaseData <- read_csv("heart_cleveland.csv", col_names = TRUE)
+datamodel<-heartDiseaseData
+datamodel$condition <- as.factor(datamodel$condition)
 #Factoring the features
 #condition
 heartDiseaseData$condition<-as.factor(heartDiseaseData$condition)
@@ -51,10 +52,13 @@ levels(heartDiseaseData$slope)[levels(heartDiseaseData$slope)==0] <- "Peak Excer
 levels(heartDiseaseData$slope)[levels(heartDiseaseData$slope)==1] <- "Peak Excercise ST Slope 1"
 levels(heartDiseaseData$slope)[levels(heartDiseaseData$slope)==2] <- "Peak Excercise ST Slope 2"
 
+#Split Train and Test
+
 
 shinyServer(function(input, output, session) {
   
   
+  # plots for data exploration tab
   output$plot1<- renderPlot({
     xvalue<-input$predictor
     if (input$plotType == "Barchart"){
@@ -92,4 +96,366 @@ shinyServer(function(input, output, session) {
       return(t)
     }
   })
+  
+  #math expression for model info tab
+  output$logReg <- renderUI({
+    
+    withMathJax(
+      helpText(
+        "$$\\ln(\\frac{p_i}{1-p_i}) = \\beta_0 + \\Sigma^k_{j=1}\\beta_jx_{ij}$$"
+      )
+    )
+  })
+  
+ # model info tab
+  output$minCpInput <- renderUI({
+    
+    ###
+    # Create the input box for the min Cp for the tree.
+    ###
+    
+    numericInput(
+      inputId = "minCp",
+      label = "Min.", 
+      min = 0, 
+      max = 1000, 
+      value = 0.01
+    )
+  })
+  output$maxCpInput <- renderUI({
+    
+    ###
+    # Create the input box for the max number of Cp in the tree.
+    ###
+    
+    # Find the user's min Cp.
+    minCp <- input$minCp
+    
+    # Start at 21.
+    value <- 10
+    
+    # If the minCp is greater than 21, move it up.
+    if (minCp > value){
+      value <- minCp
+    }
+    
+    numericInput(
+      inputId = "maxCp",
+      label = "Max.", 
+      min = minCp, 
+      max = 1000, 
+      value = value)
+  })
+  
+  observeEvent(input$trainStart, {
+    
+    ###
+    # Test the performance of the three models.
+    ###
+    
+    # Create a Progress object
+    progress <- Progress$new()
+    # Make sure it closes when we exit this reactive, even if there's an error.
+    on.exit(progress$close())
+    # Set the message to the user while cross-validation is running.
+    progress$set(message = "Performing Cross-Validation", value = 0)
+    
+    # Get the variables to use for each model.
+     Vars <- unlist(input$checkGroup)
+    
+    # Get the random seed, proportion of testing, and k-folds params.
+    randSeed <- input$randSeed
+    propTesting <- input$propTesting
+    numFolds <- input$numFolds
+    
+    # Get the Cps to try.
+    minCp <- input$minCp
+    maxCp <- input$maxCp
+    numCps <- input$numCps
+    Cps <- seq(minCp, maxCp, length.out=numCps)
+    
+    # Get the random forest mtrys.
+    randForMtry <- as.numeric(input$randForMtry)
+    
+    # Set the random seed.
+    set.seed(randSeed)
+    
+    
+    # Get the testing indexes.
+    testInd <- sample(
+      seq_len(nrow(datamodel)), 
+      size=floor(nrow(datamodel)*propTesting)
+    )
+    
+    # Split into training and testing sets.
+    train <- datamodel[-testInd, ]
+    test <- datamodel[testInd, ]
+   
+    
+    # Suppress any warning in the fitting process.
+    suppressWarnings(library(caret))
+    
+    # Set the repeated CV params.
+    TrControl <- trainControl(
+      method="cv",
+      number=numFolds
+    )
+    
+    # Increment the progress bar, and update the detail text.
+    progress$inc(0.2, detail = "Logistic Regression")
+    
+    # Evaluate the logistic regression through CV.
+    logRegModel = train(
+      condition ~ ., 
+      data=train[,c(c("condition"),Vars)],
+      method = "glm",
+      family = "binomial",
+      metric="Accuracy",
+      trControl=TrControl
+    )
+    
+    # Increment the progress bar, and update the detail text.
+    progress$inc(0.4, detail = "Classification Tree")
+    
+    # Let caret choose the best tree through CV.
+    treeModel = train(
+      condition ~ ., 
+      data=train[,c(c("condition"),Vars)],
+      method="rpart", 
+      metric="Accuracy",
+      tuneGrid=data.frame(cp = Cps),
+      trControl=TrControl
+    )
+    
+    # Increment the progress bar, and update the detail text.
+    progress$inc(0.6, detail = "Random Forest")
+    
+    # Let caret choose the best random forest through CV.
+    rfModel = train(
+      condition ~ ., 
+      data=train[,c(c("condition"),Vars)],
+      method="rf", 
+      metric= "Accuracy",
+      tuneGrid=expand.grid(mtry = randForMtry),
+      trControl=TrControl
+    )
+    
+    # Increment the progress bar, and update the detail text.
+    progress$inc(0.8, detail = "Evaluating Test Set Performance")
+
+    # Get test set predictions.
+    logRegPreds <- predict(logRegModel, test, type="raw")
+    treePreds <- predict(treeModel, test, type="raw")
+    randForPreds <- predict(rfModel, test, type="raw")
+
+    # Create the findMode function.
+    findMode <- function(x) {
+      uniqueX <- unique(x)
+      uniqueX[which.max(tabulate(match(x, uniqueX)))]
+    }
+    # 
+    # Find the no-info rate.
+     noInfoRate <- mean(findMode(test$condition) == test$condition)
+    # Find the test set performances.
+    accVec <- c(
+      noInfoRate,
+      mean(logRegPreds == test$condition, na.rm=TRUE),
+      mean(treePreds == test$condition, na.rm=TRUE),
+      mean(randForPreds == test$condition, na.rm=TRUE)
+    )
+
+    # Convert to a matrix and percentages.
+    accMatrix <- t(as.matrix(accVec)) * 100
+    # Add informative column names.
+    colnames(accMatrix) <- c(
+      "No Information Rate",
+      "Logistic Regression",
+      paste0("Tree (Cp = ", treeModel$bestTune$cp, ")"),
+      paste0("Random Forest (mtry = ", rfModel$bestTune$mtry, ")")
+    )
+    # Convert the matrix to a dataframe.
+    accTable <- as.data.frame(accMatrix) %>%
+      mutate_all(
+        round, digits = 3
+      ) %>%
+      mutate_all(
+        paste0, sep="%"
+      )
+
+   # Create the output for the accuracy table.
+   output$accTableOutput <- renderDataTable({
+     datatable(accTable)
+   })
+  
+  # Create an output for the logistic regression model rounding to 3 decimals.
+  output$logRegSummary <- renderDataTable({
+    round(as.data.frame(summary(logRegModel)$coef), 3)
+  })
+  
+  # Create a nice tree diagram.
+  output$treeSummary <- renderPlot({
+    fancyRpartPlot(treeModel$finalModel)
+  })
+
+ # Create an output for the feature importance plot for random forest model.
+  output$rfVarImpPlot <- renderPlot({
+    ggplot(varImp(rfModel, type=2)) +
+      geom_col(fill="purple") +
+      ggtitle("Most Important Features by Decrease in Gini Impurity")
+  })
+
+  # Save the fitted models in a folder.
+  saveRDS(logRegModel, "./Fitted Models/logRegModel.rds")
+  saveRDS(treeModel, "./Fitted Models/treeModel.rds")
+  saveRDS(rfModel, "./Fitted Models/rfModel.rds")
+
+})
+
+  
+  output$logRegPredInputs <- renderUI({
+    
+    ###
+    # Create a UI that lets the user input values for the logistic regression 
+    # and get a prediction.
+    ###
+    
+    # Get the variables to use for each model.
+    Vars <- input$checkGroup
+    
+    # Loop through the variables and create numeric input boxes for them. Use
+    # the median of the variable for the default value.
+    tags$ul(tagList(
+      lapply(Vars, function(variable) {
+        numericInput(
+          inputId = paste0(variable, "Value"),
+          label = paste0("Input ", variable, " Value"),
+          value = median(pull(datamodel[, variable]), na.rm=TRUE),
+          step = 0.1
+        )
+      })
+    ))
+  })
+  
+  output$treePredInputs <- renderUI({
+    
+    ###
+    # Create a UI that lets the user input values for the tree model and get a 
+    # prediction.
+    ###
+    
+    # Get the variables to use for each model.
+    Vars <- input$checkGroup
+    
+    # Loop through the variables and create numeric input boxes for them. Use
+    # the median of the variable for the default value.
+    tags$ul(tagList(
+      lapply(Vars, function(variable) {
+        numericInput(
+          inputId = paste0(variable, "Value"),
+          label = paste0("Input ", variable, " Value"),
+          value = median(pull(datamodel[, variable]), na.rm=TRUE),
+          step = 0.1
+        )
+      })
+    ))
+  })
+  
+  output$randForPredInputs <- renderUI({
+    
+    ###
+    # Create a UI that lets the user input values for the random forest model  
+    # and get a prediction.
+    ###
+    
+    # Get the variables to use for each model.
+    Vars <- input$checkGroup
+    
+    # Loop through the variables and create numeric input boxes for them. Use
+    # the median of the variable for the default value.
+    tags$ul(tagList(
+      lapply(Vars, function(variable) {
+        numericInput(
+          inputId = paste0(variable, "Value"),
+          label = paste0("Input ", variable, " Value"),
+          value = median(pull(datamodel[, variable]), na.rm=TRUE),
+          step = 0.1
+        )
+      })
+    ))
+  })
+  
+  observeEvent(input$predStart, {
+    
+    ###
+    # Return predictions when the user wants them.
+    ###
+    
+    # Retrieve the model to use for prediction.
+    modelType <- input$modelType
+    
+    # Load the appropriate model based on user input.
+    if (modelType == "logReg"){
+      
+      # Get the names of the user inputs for the logistic regression model.
+      varsOfInterest <- unlist(lapply(input$checkGroup, paste0, sep="Value"))
+      # Load in the logistic regression model.
+      myModel <- readRDS("./Fitted Models/logRegModel.rds")
+      
+    } else if (modelType == "tree"){
+      
+      # Get the names of the user inputs for the tree model.
+      varsOfInterest <- unlist(lapply(input$checkGroup, paste0, sep="Value"))
+      # Load in the tree model.
+      myModel <- readRDS("./Fitted Models/treeModel.rds")
+      
+    } else {
+      
+      # Get the names of the user inputs for the random forest model.
+      varsOfInterest <- unlist(lapply(input$checkGroup, paste0, sep="Value"))
+      # Load in the random forest model.
+      myModel <- readRDS("./Fitted Models/rfModel.rds")
+      
+    }
+    
+    # Loop through the user inputs adding them to a vector because you cannot
+    # access the variables by simply passing the vector of list elements to 
+    # input.
+    inputCopy <- c()
+    for (variable in varsOfInterest){
+      inputCopy <- c(inputCopy, input[[variable]])
+    }
+    
+    # Create a 1-row matrix of the user inputs.
+    inputCopy <- t(matrix(inputCopy))
+    # Strip "Value" from the variable names to match the column names in
+    # datamodel
+    colnames(inputCopy) <- str_remove_all(varsOfInterest, pattern="Value")
+
+    # Create a data.frame from the user inputs.
+     userInputs <- as.data.frame(inputCopy)
+
+    # Get class and probability predictions for the user inputs.
+    probPred <- predict(myModel, userInputs, type="prob")
+    # Combine them into a single matrix and round probabilities to 5 decimals.
+    preds <-  round(probPred, 5)
+   
+    # Add informative column names.
+    colnames(preds) <- c(
+      "Predicted Prob. of HeartDisease",
+      "Predicted Prob. of No HeartDisease"
+    )
+
+    # Convert the preds matrix to a data.frame.
+    preds <- as.data.frame(preds)
+
+    # Return the predictions.
+    output$preds <- renderDataTable({
+      preds
+    })
+
+  })
+
+  # Return the output.
+  return(output)
+
   })#end of shiny and function session
--- HeartDisease/ui.R
+++ HeartDisease/ui.R
@@ -6,10 +6,11 @@ library(DT)
 library(caret)
 library(tidyverse)
 library(plotly)
+library(rattle)
 
-data <- read_csv("heart_cleveland.csv", col_names = TRUE)
+heartDiseaseData <- read_csv("heart_cleveland.csv", col_names = TRUE)
+datamodel<-heartDiseaseData
 
-heartDiseaseData<-data
 #Factoring the features
 #condition
 heartDiseaseData$condition<-as.factor(heartDiseaseData$condition)
@@ -106,37 +107,7 @@ shinyUI(navbarPage(
           dataTableOutput("dTable")
         ) # close of main panel
       ), #closes the data exploration tab
-    tabPanel(
-      title="Modeling Page",
-       sidebarPanel(
-         selectInput("super",h5(strong("Choose Supervise Learning Model")),
-                     c("Linear regression model","Classification
-                       tree","Random Forest Model"))
-      ),
-      mainPanel(
-        tabsetPanel(
-          tabPanel(
-            title = "Modeling Info",
-            mainPanel(
-             verbatimTextOutput("info")
-            )
-          ),
-          tabPanel(
-            title = "Model Fitting",
-             mainPanel(
-              verbatimTextOutput("fit")
-            )
-          ),
-          tabPanel(
-            title = "Prediction",
-            mainPanel(
-             verbatimTextOutput("pred")
-            )
-          )
-        
-      )# close of subtab
-    )# close of sub main paneltab
-),# close modeling page tab
+    
     tabPanel(
       title="Data",
         sidebarPanel(
@@ -146,6 +117,246 @@ shinyUI(navbarPage(
       mainPanel(
         dataTableOutput("HDdata")
       )
-    ) #closes the data tab
-  ) #tabsetpanel
-)) #shinyUI and navbar
+    ) ,#closes the data tab
+navbarMenu(
+  
+  # Add a title.
+  title="Modeling Page",
+  
+  tabPanel(
+    title = "Modeling Info",
+    mainPanel(fluidPage(
+      # Give an overview of the modeling approaches
+      br(),
+      # Give an overview of logistic regression.
+      h4("Logistic Regression"),
+      "Logistic regression is the appropriate regression analysis to conduct",
+      "when the dependent variable is dichotomous (binary). ...",
+      "Logistic regression is used to describe data and to explain the ",
+      "relationship between one dependent binary variable and one or more nominal,",
+      "ordinal, interval or ratio-level independent variables.",
+      uiOutput("logReg"),
+      h5("Benefits"),
+      "Logistic regression is easier to implement, interpret, and very efficient to train.",
+      "It makes no assumptions about distributions of classes in feature space.",
+      "It can easily extend to multiple classes(multinomial regression) and a natural ",
+      "probabilistic view of class predictions.Good accuracy for many simple data sets",
+      "and it performs well when the dataset is linearly separable.",
+      
+      h5("Drawbacks"),
+      "The major limitation of Logistic Regression is the assumption of linearity between",
+      "the dependent variable and the independent variables.It can only be used to predict",
+      "discrete functions. Hence, the dependent variable of Logistic Regression is bound to",
+      "the discrete number set.Logistic Regression requires average or no multicollinearity",
+      "between independent variables.",
+      
+      h4("Classification Trees"),
+      "A classification tree, or simply tree, is an algorithm ",
+      "which recursively splits a feature space to create  ",
+      "regions where observations are classified by the ",
+      "most dominant class in the region. Probabilities are the ",
+      "relative frequency of each class in a terminal node.",
+      br(),
+      "Each split is made to reduce the training error as much  ",
+      "possible for that split; not for future splits. This  ",
+      "makes classification trees greedy algorithms. The best ",
+      " split at time ", strong("t"), 
+      " may not be the best split for the final ",
+      "fit. Trees are also prone to overfitting (high variance).",
+      "They do have the benefit of being highly interpretable. ",
+      br(),
+      h5("Benefits"),
+      "The Classification  methodology is one of the ",
+      "oldest and most fundamental algorithms. It is used to predict outcomes",
+      "based on certain predictor variables. They are excellent for data mining ",
+      "tasks because they require very little data pre-processing.",
+      br(),
+      h5("Drawbacks"),
+      "The disadvantage of classification tree often involves higher time to train ",
+      "the model. Decision tree training is relatively expensive as the complexity ",
+      "and time has taken are more. The Decision Tree algorithm is inadequate for ",
+      "applying regression and predicting continuous values.",
+      
+      
+      h4("Random Forests"),
+      "Random forests create bootstrap samples of the training ",
+      "data and grow classification or regression trees on each ",
+      "sample. At each split, the trees are restricted to a ",
+      "subset of the features. For classification, the trees ",
+      "take a majority vote on the class of the new data. For ",
+      "regression, their predictions are averaged.",
+      br(),
+      "Only considering a subset of features at each split ",
+      "prevents a handful of features from dominating the early ",
+      "splits in each tree and makes each tree more independent ",
+      "(hopefully). By aggregating the predictions of the ",
+      "independent trees, we reduce the variance of the ",
+      "predictions. Random forests are typically good out-of-the",
+      "-box predictive models, but unfortunately lose the ",
+      "interpretability that stand-alone trees have.",
+      br(),
+      h5("Benefits"),
+      "It is robust to outliers. It works well with non-linear data. Lower risk",
+      "of overfitting. It runs efficiently on a large dataset.",
+      br(),
+      h5("Drawbacks"),
+      "Random forests are found to be biased while dealing with categorical ",
+      "variables.It slow training method. It is not suitable for linear methods",
+      "with a lot of sparse features."
+    ))
+  ),
+  # Add a tab for fitting the models.
+  tabPanel(
+    # Add a title for the sub tab.
+    title = "Model Fitting",
+    # Allow the user to set a random seed between -1000 and 1000.
+    sidebarPanel(
+      h3("Train-Test Split"),
+      numericInput(
+        inputId = "randSeed",
+        label = "Set Random Seed",
+        value = 1,
+        min = -1000,
+        max = 1000,
+        step = 1
+      ),
+      # Allow the user to select the proportion of data to use for
+      # a testing set.
+      numericInput(
+        inputId = "propTesting",
+        label = "Proportion of Data to use for Test Set",
+        value = 0.2,
+        min = 0.1,
+        max = 0.5,
+        step = 0.05
+      ),
+      
+      # Create a section for the cross-validation parameters.
+      h3("Cross-Validation"),
+      # Set the number of folds.
+      div(
+        numericInput(
+          inputId = "numFolds",
+          label = "Number of Folds",
+          value = 3,
+          min = 3,
+          max = 5,
+          step = 1
+        )#,
+        #style="display:inline-block"
+      ),
+      
+      # Let the user set which variables to use.
+      checkboxGroupInput("checkGroup", label = h4("Let the user set which variables to use for models: "), 
+                         choices = colnames(select(datamodel, -condition)) , inline = F,
+                         selected = colnames(select(datamodel, -condition))),
+      # Add side-by-side inputs to take in the tree parameters.
+      h4(tags$b("Complexity Parameter:")),
+      div(
+        uiOutput("minCpInput"),  
+        style="display:inline-block"
+      ),
+      div(
+        uiOutput("maxCpInput"),  
+        style="display:inline-block"
+      ),
+      div(
+        numericInput(
+          inputId = "numCps",
+          label = "no. of Values", 
+          min = 1, 
+          max = 5, 
+          value = 3,
+          step = 1
+        ),  
+        style="display:inline-block"
+      ),
+      
+      
+      
+      # Let the user select the number of variables to consider
+      # at each split.
+      selectizeInput(
+        inputId = "randForMtry", 
+        label = "Select up to 5 values for mtry:", 
+        choices = 1:length(colnames(datamodel)[1:13]),
+        multiple = TRUE,
+        selected = c(2, 6, 8),
+        options = list(maxItems = 5)
+      ),
+      # Add a button for fitting models.
+      actionButton(
+        inputId = "trainStart",
+        label = "Fit Models"
+      )
+    ),
+    # Create the main panel to hold model performances and 
+    # summaries.
+    mainPanel(
+      # Show the test-set accuracy.
+      h3("Test Set Accuracies to 3 decimal places"),
+      dataTableOutput("accTableOutput"),
+      br(),
+      # Show the coefficients of the Logistic Regression Model.
+      h3("Summary of Linear Regression Model"),
+      dataTableOutput("logRegSummary"),
+      tableOutput('logRegconfusionMatrix'),
+      verbatimTextOutput("logRegclassError"),
+      br(),
+      # Show the final tree diagram.
+      h3("Classification Tree Diagram"),
+      plotOutput("treeSummary"),
+      tableOutput('treeconfusionMatrix'),
+      verbatimTextOutput("treeclassError"),
+      br(),
+      h3("Random Forest Feature Importances"),
+      plotOutput("rfVarImpPlot"),
+      verbatimTextOutput("rfVarclassError")
+    )
+  ),
+  
+  # Create the prediction tab.
+  tabPanel(
+    # Add a title.
+    title = "Prediction",
+    # Create a sidebar for the user to play with inputs.
+    sidebarPanel(
+      # Add buttons to select which model to use.
+      radioButtons(
+        inputId = "modelType",
+        label = "Choose a Model",
+        inline = TRUE,
+        choiceNames = c("Logistic Regression","Classification Tree","Random Forest"),
+        choiceValues = c("logReg", "tree", "randFor"),
+        selected = "logReg"
+      ),
+      # Add a button for fitting models.
+      actionButton(
+        inputId = "predStart",
+        label = "Predict"
+      ),
+      # Depending on which model to use for prediction, change the
+      # variables shown to match the fitted models.
+      conditionalPanel(
+        condition = "input.modelType == 'logReg'",
+        uiOutput("logRegPredInputs")
+      ),
+      conditionalPanel(
+        condition = "input.modelType == 'tree'",
+        uiOutput("treePredInputs")
+      ),
+      conditionalPanel(
+        condition = "input.modelType == 'randFor'",
+        uiOutput("randForPredInputs")
+      )
+    ),
+    # Create the main panel to show predictions.
+    mainPanel(
+      h3("Predicted Heart disease of a person with selected Inputs"),
+      dataTableOutput("preds")
+    )
+  )
+)
+)
+)
+)
